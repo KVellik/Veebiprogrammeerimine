a3cf618761db1869669f9c911968acde6ab683be
Karl Vellik <KVellik@users.noreply.github.com>
2019-09-10 11:34:42 +0300

    Lisatud kellaaja kontroll

--- 02 tund (10.09.19)/testpage.php
+++ 02 tund (10.09.19)/testpage.php
@@ -1,10 +1,19 @@
 <?php
   $userName = "Karl Vellik";
+  $fullTimeNow = date("d.m.Y H:i:s");
+  $hourNow = date("H");
+  $partOfDay = "hägune aeg";
+  
+  if($hourNow < 8){
+	  $partOfDay = "hommik";
+
+  }	  
 ?>
+
 <!DOCTYPE html>
 <html lang="et">
 <head>
-  <meta charset="UTF-8">
+  <meta charset="UTF-8"/>
   <title>
   <?php
   echo $userName; 
@@ -17,7 +26,8 @@
   echo "<h1>" .$userName .", veebiprogrammeerimine</h1>";
   ?>
   <p>See veebileht on loodud õppetöö käigus ning ei oma mingit tõsist sisu!</p>
-  <em>Italic tekst</em>
-  <u>Test tekst</u>
+  <?php
+  echo "<p>Lehe avamise hektel oli aeg: " .$fullTimeNow .", " .$partOfDay .".</p>";
+  ?>
 </body>
 </html>
