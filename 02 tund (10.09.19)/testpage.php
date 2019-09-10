5b41e17994915eedf4caf7b40281b967d311c6af
Karl Vellik <KVellik@users.noreply.github.com>
2019-09-10 11:09:24 +0300

    esimene üleslaadimine

---
+++ 02 tund (10.09.19)/testpage.php
@@ -0,0 +1,23 @@
+<?php
+  $userName = "Karl Vellik";
+?>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+  <meta charset="UTF-8">
+  <title>
+  <?php
+  echo $userName; 
+  ?>  
+  programeerib veebi</title>
+
+</head>
+<body>
+  <?php
+  echo "<h1>" .$userName .", veebiprogrammeerimine</h1>";
+  ?>
+  <p>See veebileht on loodud õppetöö käigus ning ei oma mingit tõsist sisu!</p>
+  <em>Italic tekst</em>
+  <u>Test tekst</u>
+</body>
+</html>
